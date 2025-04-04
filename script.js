ffbb78fa19351188bbbdfe2915700a3618e7d78f
christeena211 <christeenamary39@gmail.com>
2025-04-04 16:00:22 +0530

    delete

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("MyG Page Loaded!");
-
-    // Example Function: Alert when clicking "Manage Products"
-    document.querySelector('.btn-warning').addEventListener('click', (event) => {
-        event.preventDefault();
-        window.location.href = 'products.html';
-    });
-});
